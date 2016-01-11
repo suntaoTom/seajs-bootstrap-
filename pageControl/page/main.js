2db1f62f0246051da66c557fcbfd0060f8bfe0c6
suntaoTom <459269440@qq.com>
2016-01-11 18:30:57 +0800

    分页器

---
+++ pageControl/page/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by wb-sundaotao.d on 2016-01-04.
+ */
+define(function (require,exports,module) {
+
+    var main={
+        //查询
+        queryResult:function(pagenum){
+            console.log(pagenum)
+        }
+    }
+    module.exports= main;
+})
